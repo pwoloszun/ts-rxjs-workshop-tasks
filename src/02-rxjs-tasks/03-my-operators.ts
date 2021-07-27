99a149d89bcc1524db98398dab1770d69cdba4c3
pwoloszun <piotr.woloszun@gmail.com>
2021-07-27 14:16:13 +0200

    minors

--- src/02-rxjs-tasks/03-my-operators.ts
+++ src/02-rxjs-tasks/03-my-operators.ts
@@ -312,43 +312,7 @@ export function myOperatorsApp() {
 }
 
 
-myFilter$(
-  myMap$(
-    myInterval$(1000),
-    (i) => i ** 2
-  ),
-  (n) => n % 2 === 0
-)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function f() {
-  // do something
-};
-
-function g() {
-
-}
-
-
-function other() {
-  const resultG = g();
-  const fResult = f();
-  return resultG + fResult + 10;
-};
 
 
 
