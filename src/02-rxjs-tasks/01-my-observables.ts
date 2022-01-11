900210bfe21f55e53567c4db9a85c1f3b7b6706c
pwoloszun <piotr.woloszun@gmail.com>
2022-01-11 10:46:55 +0100

    from array task

--- src/02-rxjs-tasks/01-my-observables.ts
+++ src/02-rxjs-tasks/01-my-observables.ts
@@ -92,14 +92,25 @@ function myFullObserver(tag: string): Observer<any> {
 }
 
 // TODO task: myFromArray$
-export function myFromArray$(items: any[]): Observable<any> {
+export function myFromArray$<T>(items: T[]): Observable<T> {
   return NEVER; // TODO
 }
 
 function fromArrayTask() {
   const names = ['bob', 'ed', 'kate'];
-  myFromArray$(names)
-    .subscribe(myFullObserver('fromArrayTask'));
+  const names$ = myFromArray$(names)
+  names$.subscribe({
+    next(value) {
+      console.log('NEXT fromArrayTask', value);
+    },
+    error(err) {
+      console.log('ERROR fromArrayTask', err);
+    },
+    complete() {
+      console.log('COMPLETE fromArrayTask');
+    },
+
+  });
 }
 
 // TODO task: myRange$
@@ -188,8 +199,8 @@ function throwTask() {
 export function myObservablesApp() {
   // example1();
   // timeoutTask();
-  intervalTask();
-  // fromArrayTask();
+  // intervalTask();
+  fromArrayTask();
   // fromArrayWithDelayTask();
   // throwTask();
   // rangeTask();
