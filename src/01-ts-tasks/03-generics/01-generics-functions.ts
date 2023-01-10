a4a40ad722d4ad03e2e2ead8193e521e9661654f
pwoloszun <piotr.woloszun@gmail.com>
2023-01-10 08:21:33 +0100

    generic functions tasks

--- src/01-ts-tasks/03-generics/01-generics-functions.ts
+++ src/01-ts-tasks/03-generics/01-generics-functions.ts
@@ -1,79 +1,33 @@
 // TODO 1: MapIterator interface; gMap function
-interface MapIterator<T, K> {
-  (el: T, index: number): K;
-}
-
-function gMap<T, K>(list: T[], iterator: MapIterator<T, K>): K[] {
-  let mappedList: K[] = [];
-  for (let i: number = 0; i < list.length; i++) {
-    mappedList.push(iterator(list[i], i));
-  }
-  return mappedList;
+function gMap(list: any[], iterator: Function): any[] {
+  return [] as any;
 }
 
 // TODO 2: FilterIterator interface; gFilter function
-interface FilterIterator<T> {
-  (el: T, index: number): boolean;
-}
-
-function gFilter<T>(list: T[], iterator: FilterIterator<T>): T[] {
-  let filteredList: T[] = [];
-  for (let i: number = 0; i < list.length; i++) {
-    let el: T = this[i];
-    if (iterator(el, i)) {
-      filteredList.push(el);
-    }
-  }
-  return filteredList;
+function gFilter(list: any[], iterator: Function): any[] {
+  return [] as any;
 }
 
 // TODO 3: AllIterator interface; gAll function
-interface AllIterator<T> {
-  (el: T, index: number): boolean;
-}
-
-function gAll<T>(list: T[], iterator: AllIterator<T>): boolean {
-  let item: T;
-  for (let i: number = 0; i < list.length; i++) {
-    item = list[i];
-    if (!iterator(item, i)) {
-      return false;
-    }
-  }
+function gAll(list: any[], iterator: Function): boolean {
   return true;
 }
 
 // TODO 4: SliceIterator interface; gEachSlice function
-interface SliceIterator<T> {
-  (elementsSlice: T[], index: number): void;
-}
-
-function gEachSlice<T>(list: T[], sliceIterator: SliceIterator<T>, sliceSize: number): void {
+function gEachSlice(list: any[], sliceIterator: Function, sliceSize: number): void {
 }
 
 // TODO 5: ReduceIterator interface; gReduce function
-interface ReduceIterator<T, K> {
-  (memo: K, element: T, index: number, list: T[]): K;
-}
-
-function gReduce<A, B>(list: A[], iterator: ReduceIterator<A, B>, memo: B): B {
-  return null;
+function gReduce(list: any[], iterator: Function, memo: any): any {
+  return null as any;
 }
 
 // TODO 6: gPluck function
-function gPluck<T>(list: Object[], propertyName: string): T[] {
+function gPluck(list: any[], propertyName: string): any[] {
   return [];
 }
 
 // TODO 7: interfaces: GroupByIterator & Grouped; gGroupBy function
-interface GroupByIterator<T> {
-  (el: T, index: number): string;
-}
-
-interface Grouped<T> {
-  [key: string]: T[];
-}
-
-function gGroupBy<T>(list: T[], iterator: GroupByIterator<T>): Grouped<T> {
-  return {};
+function gGroupBy(list: any[], iterator: Function): any {
+  return {} as any;
 }
